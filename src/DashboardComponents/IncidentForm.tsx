23f2be9b75cf008fa959fafe94d5efef31297c73
ChetanAnandaReddyKukutla <chetananandareddykukutla@gmail.com>
2025-05-04 16:08:36 +0530

    minorUpdate

--- src/DashboardComponents/IncidentForm.tsx
+++ src/DashboardComponents/IncidentForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { SeverityLevel, mockIncidents } from "../Data/mockIncidents";
 import { toast } from "sonner";
 
